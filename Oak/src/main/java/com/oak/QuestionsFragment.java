0ba315049980a78000b1210c1eac5b88efeae223
Amandeep Grewal <me@amandeep.ca>
2013-08-26 21:04:04 -0400

    Optimize imports.

--- Oak/src/main/java/com/oak/QuestionsFragment.java
+++ Oak/src/main/java/com/oak/QuestionsFragment.java
@@ -14,7 +14,6 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
